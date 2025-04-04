81e47fb06684e9febe8b27068259650c63b96f6c
ahmedfahmy <ahmedbnfahmy@gmail.com>
2025-04-04 21:11:16 +0200

    nav styling

--- src/components/Headers/Header.tsx
+++ src/components/Headers/Header.tsx
@@ -21,45 +21,52 @@ const Header: React.FC = () => {
   return (
     <header className="header" data-testid="header">
       <nav className="header__nav">
-        {/* Logo/Home Link */}
-        <Link
-          to="/"
-          className="header__logo"
-          data-testid="header-logo"
-          onClick={() => setActiveCategory('all')}
-        >
-          MyStore
-        </Link>
-
-        {/* Category Navigation */}
-        <ul className="header__categories">
-          {categories.map((category) => (
-            <li key={category.id}>
-              <Link
-                to={`/${category.id}`}
-                className={`header__category-link ${activeCategory === category.id ? 'header__category-link--active' : ''
+        {/* Category Navigation (Left) */}
+        <div className="header__nav-left">
+          <ul className="header__categories">
+            {categories.map((category) => (
+              <li key={category.id}>
+                <Link
+                  to={`/${category.id}`}
+                  className={`header__category-link ${
+                    activeCategory === category.id ? 'header__category-link--active' : ''
                   }`}
-                data-testid={
-                  activeCategory === category.id
-                    ? 'active-category-link'
-                    : 'category-link'
-                }
-                onClick={() => setActiveCategory(category.id)}
-              >
-                {category.name}
-              </Link>
-            </li>
-          ))}
-        </ul>
+                  data-testid={
+                    activeCategory === category.id
+                      ? 'active-category-link'
+                      : 'category-link'
+                  }
+                  onClick={() => setActiveCategory(category.id)}
+                >
+                  {category.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
 
-        {/* Cart Button */}
-        <button
-          className="header__cart-btn"
-          onClick={() => setIsCartOpen(!isCartOpen)}
-          data-testid="cart-btn"
-        >
-          <ShoppingCartIcon />
-        </button>
+        {/* Logo (Center) */}
+        <div className="header__nav-center">
+          <Link
+            to="/"
+            className="header__logo"
+            data-testid="header-logo"
+            onClick={() => setActiveCategory('all')}
+          >
+            MyStore
+          </Link>
+        </div>
+
+        {/* Cart Button (Right) */}
+        <div className="header__nav-right">
+          <button
+            className="header__cart-btn"
+            onClick={() => setIsCartOpen(!isCartOpen)}
+            data-testid="cart-btn"
+          >
+            <ShoppingCartIcon />
+          </button>
+        </div>
       </nav>
     </header>
   );
